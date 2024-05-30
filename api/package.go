ad5d82eb3ccb563bd122582f3c442fd69f0c8953
Dan Esparza <danesparza@cagedtornado.com>
2024-05-30 19:04:52 -0400

    Add debug logging

--- api/package.go
+++ api/package.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/danesparza/package-assistant/internal/debian"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 	"io"
 	"net/http"
@@ -37,6 +38,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	githubPassword := viper.GetString("github.password")
 
 	//	First check the auth token and make sure it exists on the header:
+	log.Debug().Msg("Validating X-PackAuth header")
 	authToken := req.Header.Get("X-PackAuth")
 	if strings.TrimSpace(authToken) != strings.TrimSpace(viper.GetString("auth.token")) {
 		err := fmt.Errorf("X-PackAuth token invalid")
@@ -45,6 +47,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	}
 
 	//	First check for maximum uplooad size and return an error if we exceed it.
+	log.Debug().Int64("MAX_UPLOAD_SIZE", MAX_UPLOAD_SIZE).Msg("Checking size vs max upload size")
 	req.Body = http.MaxBytesReader(rw, req.Body, MAX_UPLOAD_SIZE)
 	if err := req.ParseMultipartForm(MAX_UPLOAD_SIZE); err != nil {
 		err = fmt.Errorf("uploaded file is too big: %w", err)
@@ -52,9 +55,10 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 		return
 	}
 
-	// FormFile returns the first file for the given key `myFile`
+	// FormFile returns the first file for the given key `file`
 	// it also returns the FileHeader so we can get the Filename,
 	// the Header and the size of the file
+	log.Debug().Msg("Checking for 'file' form element and fetching")
 	file, fileHeader, err := req.FormFile("file")
 	if err != nil {
 		err = fmt.Errorf("error retrieving file from the 'file' form element: %w", err)
@@ -65,6 +69,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 
 	// Create the uploads folder if it doesn't
 	// already exist
+	log.Debug().Str("UploadPath", UploadPath).Msg("Creating upload path if it doesn't exist")
 	err = os.MkdirAll(UploadPath, os.ModePerm)
 	if err != nil {
 		err = fmt.Errorf("error creating uploads path: %w", err)
@@ -74,6 +79,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 
 	// Create a new file in the uploads directory
 	destinationFile := path.Join(UploadPath, fileHeader.Filename)
+	log.Debug().Str("destination file", destinationFile).Msg("Creating file in uploads directory")
 	dst, err := os.Create(destinationFile)
 	if err != nil {
 		err = fmt.Errorf("error creating file: %w", err)
@@ -84,6 +90,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 
 	// Copy the uploaded file to the filesystem
 	// at the specified destination
+	log.Debug().Msg("Copying file data to the destination file")
 	_, err = io.Copy(dst, file)
 	if err != nil {
 		err = fmt.Errorf("error saving file: %w", err)
@@ -94,6 +101,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	//	Process the file
 
 	//	ci-pre.sh (switch to repo folder and git pull)
+	log.Debug().Msg("Performing a repo pull")
 	err = service.RepoSvc.Pull()
 	if err != nil {
 		err = fmt.Errorf("error refreshing repo: %w", err)
@@ -103,6 +111,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 
 	//	Move file to repo folder
 	repoFile := path.Join(RepoPath, fileHeader.Filename)
+	log.Debug().Str("repoFile", repoFile).Msg("Moving file to the repo path")
 	err = os.Rename(destinationFile, repoFile)
 	if err != nil {
 		err = fmt.Errorf("error moving file to repo: %w", err)
@@ -111,6 +120,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	}
 
 	//  ci-refresh.sh / refresh-packages.sh (Perform dpkg-scanpackages, gzip and sign using gpg)
+	log.Debug().Msg("Refreshing packages")
 	err = debian.RefreshPackages(req.Context(), gpgPassword, gitEmail, RepoPath)
 	if err != nil {
 		err = fmt.Errorf("error refreshing packages: %w", err)
@@ -119,6 +129,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	}
 
 	//	ci-post.sh (git add / git commit / git push)
+	log.Debug().Msg("Adding all changes and preparing to commit")
 	err = service.RepoSvc.AddAll()
 	if err != nil {
 		err = fmt.Errorf("error adding changes in repo: %w", err)
@@ -126,6 +137,7 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 		return
 	}
 
+	log.Debug().Msg("Committing and pushing changes")
 	err = service.RepoSvc.CommitAndPush(githubUser, githubPassword, gitName, gitEmail)
 	if err != nil {
 		err = fmt.Errorf("error committing and pushing: %w", err)
@@ -142,4 +154,5 @@ func (service Service) UploadPackage(rw http.ResponseWriter, req *http.Request) 
 	rw.WriteHeader(http.StatusCreated)
 	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
 	json.NewEncoder(rw).Encode(response)
+	log.Debug().Msg("Complete!")
 }
